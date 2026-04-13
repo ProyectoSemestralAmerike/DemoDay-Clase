6ab3825ef7654aafca710edb6c7b709273308f34
ProyectoSemestralAmerike <calvarado@amerike.edu.mx>
2026-04-13 11:40:50 -0600

    Se añadió la variable lifePoints
    El jugador tiene 50 puntos de vida

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int lifePoints = 50;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
