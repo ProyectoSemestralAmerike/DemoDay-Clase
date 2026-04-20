c959591330aa8daab9ba94bb5bd5930605380a80
ProyectoSemestralAmerike <calvarado@amerike.edu.mx>
2026-04-20 12:38:49 -0600

    Added Main Menu Scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
